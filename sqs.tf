b683ec2580d69c55a59c892fb9cae2335f6c723c
Ellie0797 <elliegwon7@gmail.com>
2024-04-28 14:36:47 +0100

    added gitaction file

---
+++ sqs.tf
@@ -0,0 +1,19 @@
+resource "aws_sqs_queue" "queue" {
+  name = "${var.resource_name}-s3-event-notification-queue"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:*:*:${var.resource_name}-s3-event-notification-queue",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.bucket.arn}" }
+      }
+    }
+  ]
+}
+POLICY
+}
